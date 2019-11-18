d676e4a79faecd01b86ff502f21f60c85b625cdc
anotherL <anotherl@LdeMacBook-Pro.local>
2019-11-18 15:48:09 +0800

    更新维护部app下载页

--- apps/maintainProject/js/install.js
+++
@@ -1,4 +0,0 @@
-function openInstallURL() {
-    var url = 'itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/anotherCPL/appDownloadTest/master/apps/maintainProject/install.plist';
-  window.self.location = url;
-}
